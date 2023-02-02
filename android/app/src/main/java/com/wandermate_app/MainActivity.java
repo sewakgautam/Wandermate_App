4d8454fad3de4bc61efd5cf352e11639d464f74b
sewakgautam <sewak.gautam58@gmail.com>
2023-02-03 02:18:10 +0545

    🐞 fix: Change Name
    Changed App Name from wandermate_app to Wanderamte

--- android/app/src/main/java/com/wandermate_app/MainActivity.java
+++ android/app/src/main/java/com/wandermate_app/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "wandermate_App";
+    return "Wandermate";
   }
 
 
--- ios/wandermate_AppTests/wandermate_AppTests.m
+++ ios/wandermate_AppTests/wandermate_AppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface wandermate_AppTests : XCTestCase
+@interface WandermateTests : XCTestCase
 
 @end
 
-@implementation wandermate_AppTests
+@implementation WandermateTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
