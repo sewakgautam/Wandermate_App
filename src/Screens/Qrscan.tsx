f293d493b14aabba1b63be122aa10574f4c6cb76
sewakgautam <sewak.gautam58@gmail.com>
2023-03-10 15:29:42 +0545

    Implement QRcode reader

--- src/Screens/Qrscan.tsx
+++ src/Screens/Qrscan.tsx
@@ -1,36 +1,51 @@
-import {Image, Text, View} from 'react-native';
+import {
+  Alert,
+  Image,
+  Linking,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import {color} from '../config/constraint';
 
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import {RNCamera} from 'react-native-camera';
+
 export default function Qrscan() {
+  const scannedData = t => {
+    console.log(t.data);
+    Alert.alert(t.data);
+  };
   return (
     <View style={{backgroundColor: color.Background, flex: 1}}>
-      <View
-        style={{marginHorizontal: 30, marginTop: 250, alignItems: 'center'}}>
-        <Image
-          source={require('../../assets/img/DesignAsset/sadcamera.png')}
-          style={{height: 200, width: 250}}
-        />
-        <Text
-          style={{
-            // color: 'black',
-            fontWeight: 'bold',
-            fontSize: 25,
-            textAlign: 'center',
-          }}>
-          Ooops!! we Cannot Access your camera.
-        </Text>
-        <Text
-          style={{
-            top: 10,
-            // color: 'black',
-            fontWeight: '400',
-            fontSize: 15,
-            textAlign: 'center',
-          }}>
-          Keep it quiet! The developer of our application didn't know how to
-          access the camera 😉
-        </Text>
-      </View>
+      <QRCodeScanner
+        markerStyle={{borderColor: color.Primary}}
+        fadeIn={true}
+        onRead={t => scannedData(t)}
+        showMarker={true}
+        reactivate={true}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+  buttonTouchable: {
+    padding: 16,
+  },
+});
